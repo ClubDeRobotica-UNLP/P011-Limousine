1b4fccd267d89a6961c13bbab0832f622f484f78
CDR-PC <cdr.fiunlp@gmail.com>
2016-05-11 12:20:11 -0300

    Implementado nuevo control, luces y balizas

--- Arduino/azulPicasso/azulPicasso.ino
+++ Arduino/azulPicasso/azulPicasso.ino
@@ -4,8 +4,15 @@
 #define M2A 11
 #define LED1 2
 #define LED2 7
+#define M1E 8
+#define M2E 13
+
+#include "Timer.h"
 
 char Buffer[2] = {0, 0}; // Buffer[0] : Adelante, Buffer[1] : Giro.
+byte balFlag = 0;
+byte lightsOn = 0;
+Timer t;
 
 void setup()
 {
@@ -13,70 +20,122 @@ void setup()
   pinMode(LED2, OUTPUT);
   digitalWrite(LED1, 0);
   digitalWrite(LED2, 0);
+  t.every(1000, blinkLights);
 
-  pinMode(8, OUTPUT);
-  pinMode(13, OUTPUT);
-  digitalWrite(8, 1);
-  digitalWrite(13, 1);
-  
   delay(500);
-  
-  MotoresInit(M1A, M2A);
+
+  MotoresInit(M1A, M2A, M1E, M2E);
   Serial.begin(9600);
-  ApagarMotores(M1PWM, M1A, M2PWM, M2A);  
+  ApagarMotores(M1PWM, M1A, M2PWM, M2A);
 }
 
 void loop()
 {
-  Buffer[0] = 0;
-  delay(40);
-  if (Serial.available() > 0)
+  t.update();
+  if ( Serial.available() > 0 )
   {
     Serial.readBytes(&Buffer[0], 1);
-  }
-  switch (Buffer[0])
-  {
-    case 'a':
-      digitalWrite(LED1, 1);
-      digitalWrite(LED2, 1);
-      MoverMotores(M1PWM, M1A, M2PWM, M2A, 200, 200);
-      break;
+    //Serial.flush();
 
-    case 'b':
-      digitalWrite(LED1, 0);
-      digitalWrite(LED2, 0);
-      MoverMotores(M1PWM, M1A, M2PWM, M2A, -200, -200);
-      break;
+    switch (Buffer[0])
+    {
+      case 'F':
+        MoverMotores(M1PWM, M1A, M2PWM, M2A, 200, 200);
+        break;
 
-    case 'c':
-      digitalWrite(LED1, 1);
-      digitalWrite(LED2, 0);
-      MoverMotores(M1PWM, M1A, M2PWM, M2A, 200, -100);
-      break;
+      case 'B':
+        MoverMotores(M1PWM, M1A, M2PWM, M2A, -200, -200);
+        break;
 
-    case 'd':
-      digitalWrite(LED1, 0);
-      digitalWrite(LED2, 1);
-      MoverMotores(M1PWM, M1A, M2PWM, M2A, -100, 200);
-      break;
+      case 'L':
+        MoverMotores(M1PWM, M1A, M2PWM, M2A, 200, -200);
+        break;
 
-    case 'e':
-    default:
-      digitalWrite(LED1, 0);
-      digitalWrite(LED2, 0);
-      ApagarMotores(M1PWM, M1A, M2PWM, M2A);
-      break;
+      case 'R':
+        MoverMotores(M1PWM, M1A, M2PWM, M2A, -200, 200);
+        break;
+
+      case 'G': /*Forward left*/
+        MoverMotores(M1PWM, M1A, M2PWM, M2A, 200, 100);
+        break;
+
+      case 'I': /*  Forward Right */
+        MoverMotores(M1PWM, M1A, M2PWM, M2A, 100, 200);
+        break;
+
+      case 'H': /*  Back Left  */
+        MoverMotores(M1PWM, M1A, M2PWM, M2A, -200, -100);
+        break;
+
+      case 'J':  /* Back Right  */
+        MoverMotores(M1PWM, M1A, M2PWM, M2A, -100, -200);
+        break;
+
+      case 'W': /* Leds on */
+        digitalWrite(LED1, 1);
+        digitalWrite(LED2, 1);
+        lightsOn = 1;
+        break;
+
+      case 'w': /* Leds off */
+        digitalWrite(LED1, 0);
+        digitalWrite(LED2, 0);
+        lightsOn = 0;
+        break;
+
+      case 'X': /* Balizas  */
+        balFlag = 1;
+        break;
+
+      case 'x': /* Balizas off */
+        balFlag = 0;
+        if(!lightsOn)
+        {
+          digitalWrite(LED1, 0);
+          digitalWrite(LED2, 0);
+        }
+        break;
+
+      case 'S':
+      default:
+        ApagarMotores(M1PWM, M1A, M2PWM, M2A);
+        break;
+    }
   }
 }
+
+
+/*---------------------------------------------------------------------------------
+ * Timer Balizas 
+ *----------------------------------------------------------------------------------*/
+void blinkLights()
+{
+  if(balFlag && !lightsOn)
+  {
+    if(digitalRead(LED1))
+    {
+      digitalWrite(LED1,0);
+      digitalWrite(LED2,0);  
+    } else {
+      digitalWrite(LED1,1);
+      digitalWrite(LED2,1);    
+    }    
+  }
+}
+
 
 /*----------------------------------------------------------------------------------
     Función para inicializar los Motores
   ----------------------------------------------------------------------------------*/
 
-void MotoresInit(byte motor1D, byte motor2D)
+void MotoresInit(byte motor1D, byte motor2D, byte motor1E, byte motor2E)
 {
   pinMode(motor1D, OUTPUT);
   pinMode(motor2D, OUTPUT);
+  pinMode(motor2E, OUTPUT);
+  pinMode(motor2E, OUTPUT);
+  digitalWrite(motor2E, 1);
+  digitalWrite(motor2E, 1);
 }
 
 /*----------------------------------------------------------------------------------
